2caf948b05f57e878020d2ced996534fe9d035fd
JoshKoiro <josh.koiro@gmail.com>
2017-11-14 18:48:33 -0500

    added testing library

--- testing-library/script.js
+++ testing-library/script.js
@@ -1,3 +1,20 @@
-module.exports.test = () => {
-    
+const log = require('../color-console/script.js').log
+const color = require('../color-console/script.js').colors
+
+module.exports.render = (val,text) => {
+    (val === true)
+        ? log(color.FgGreen,text)
+        : log(color.FgRed,text)
+}
+
+module.exports.equal = (func,val,text) => {
+    (func === val)
+        ? this.render(true,text)
+        : this.render(false,text)
+} 
+
+module.exports.not = (func,val,text) => {
+    (func !== val)
+        ? this.render(true,text)
+        : this.render(false,text)
 }
--- testing-library/test.js
+++ testing-library/test.js
@@ -1,2 +1,8 @@
-const test = require('./script.js')
+const tester = require('./script.js')
+
+const testFun = (a) => (a === undefined) ? 1 : a * 2
+
+tester.equal(testFun(),1,"test function returns 1")
+
+tester.equal(testFun(5),10,"test function multiplies by 2")
 
